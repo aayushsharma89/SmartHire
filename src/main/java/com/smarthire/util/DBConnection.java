7ba1ede0d3f761c4c8140eea08c26b79328e0cb5
Aayush Sharma <aayushsharma8958@gmail.com>
2026-09-03 11:21:49 +0530

    Configure MySQL SSL for Aiven database

--- src/main/java/com/smarthire/util/DBConnection.java
+++ src/main/java/com/smarthire/util/DBConnection.java
@@ -19,27 +19,20 @@ public class DBConnection {
 
     public static Connection getConnection() {
 
-        String host = getEnv("DB_HOST", "localhost");
-        String port = getEnv("DB_PORT", "3306");
-        String database = getEnv("DB_NAME", "smarthire");
-        String user = getEnv("DB_USER", "root");
-
-        String password = System.getenv("DB_PASSWORD");
-
-        if (password == null || password.isBlank()) {
-            throw new RuntimeException(
-                "DB_PASSWORD environment variable is not set."
-            );
-        }
-
-        String url =
-                "jdbc:mysql://" + host + ":" + port + "/" + database
-                + "?useSSL=false&allowPublicKeyRetrieval=true"
-                + "&serverTimezone=UTC";
-
         try {
 
             Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String host = getEnv("DB_HOST", "localhost");
+            String port = getEnv("DB_PORT", "3306");
+            String database = getEnv("DB_NAME", "smarthire");
+            String user = getEnv("DB_USER", "root");
+            String password = System.getenv("DB_PASSWORD");
+
+            String url =
+                    "jdbc:mysql://" + host + ":" + port + "/" + database
+                    + "?sslMode=REQUIRED"
+                    + "&serverTimezone=UTC";
 
             Connection connection =
                     DriverManager.getConnection(
@@ -57,8 +50,7 @@ public class DBConnection {
         } catch (ClassNotFoundException e) {
 
             throw new RuntimeException(
-                    "MySQL JDBC Driver not found. "
-                    + "Check mysql-connector-j JAR.",
+                    "MySQL JDBC Driver not found.",
                     e
             );
 
